b2fe66ea72f404f214fb696c1d5ea6b5f2a7e050
Furkan Çat <furi544@gmail.com>
2023-05-17 02:37:28 +0300

    changed route logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,29 @@ import {
   createRoutesFromElements,
   Route,
   RouterProvider,
+  Navigate,
 } from "react-router-dom";
-import Product from "./pages/Product";
+import Products from "./pages/Products";
 import ProductDetail from "./pages/ProductDetail";
-import AppLayout from "./layout/AppLayout";
+import ProductLayout from "./pages/ProductLayout";
+import { Navbar } from "./components";
 
 export const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-        <Route path="/product" element={<AppLayout />}>
-          <Route index element={<Product />} />
-          <Route path="product/:id" element={<ProductDetail />} />
+        <Route path="/" element={<ProductLayout />}>
+          <Route index element={<Products />} />
         </Route>
+        <Route path="/products" element={<Navigate to="/" />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
       </Route>
     )
   );
 
   return (
     <>
+      <Navbar />
       <RouterProvider router={router} />
     </>
   );
