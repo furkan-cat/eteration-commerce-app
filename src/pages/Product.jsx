5d5d5855f7c69b15d22ec803e5c406a8bae77974
Furkan Çat <furi544@gmail.com>
2023-05-17 02:41:59 +0300

    changed layout & products component names

--- src/pages/Product.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Home() {
-  return (
-    <div className="text-3xl font-bold underline bg-indigo-500 h-full">
-      <div className="container">Home</div>
-    </div>
-  );
-}
