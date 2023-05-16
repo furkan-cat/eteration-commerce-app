f0921b025a7b9381d60068bf6e5ca4576ff06b5b
Furkan Çat <furi544@gmail.com>
2023-05-17 02:39:00 +0300

    navbar improvements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,26 @@
 import { BsBag } from "react-icons/bs";
 import { AiOutlineUser } from "react-icons/ai";
+import { BiSearchAlt } from "react-icons/bi";
+import Input from "./Input/Input";
 
 export default function Navbar() {
   return (
-    <div className="bg-blue-500 text-white p-3">
+    <div className="bg-blue-500 text-white p-3 flex-1">
       <div className="container flex justify-between">
-        <div className="flex items-center space-x-4">
+        <div className="flex sm:flex-row flex-col sm:items-center items-start sm:space-x-2 md:space-x-4">
           <div>Eteration</div>
-          <input className="" onChange={() => null} placeholder="Search" />
+          <Input prefix={<BiSearchAlt />} placeholder="Search" />
         </div>
 
-        <div className="flex items-center space-x-4">
-          <BsBag />
-          <div>118.123TL</div>
-          <AiOutlineUser />
+        <div className="flex sm:flex-row flex-col sm:items-center items-start sm:space-x-2 md:space-x-4">
+          <div className="flex items-center sm:space-x-2">
+            <BsBag />
+            <div>118.123TL</div>
+          </div>
+          <div className="flex items-center sm:space-x-2">
+            <AiOutlineUser />
+            <div>Kerem</div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 export { default as Navbar } from "./Navbar";
 export { default as Card } from "./Card/Card";
-export { default as CardHeader } from "./Card/CardHeader";
 export { default as CardBody } from "./Card/CardBody";
-export { default as CardFooter } from "./Card/CardFooter";
+export { default as Radio } from "./Radio/Radio";
+export { default as CheckBox } from "./CheckBox/CheckBox";
+export { default as ProductCard } from "./ProductCard/ProductCard";
+export { default as QuantityPill } from "./QuantityPill/QuantityPill";
+export { default as LineItem } from "./LineItem/LineItem";
