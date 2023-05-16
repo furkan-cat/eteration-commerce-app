95e7e5f9885b794c0760d4bae0b95cd6495dfb82
Furkan Çat <furi544@gmail.com>
2023-05-16 21:07:03 +0300

    product page initial

---
+++ src/pages/Product.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="text-3xl font-bold underline bg-indigo-500 h-full">
+      <div className="container">Home</div>
+    </div>
+  );
+}
