82acdecca1c1e5845e46bc42081e32d4e07ebc81
Furkan Çat <furi544@gmail.com>
2023-05-17 02:39:55 +0300

    added input component

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+import cn from "classnames";
+
+export default function Input({ name, value, onChange, prefix, placeholder }) {
+  return (
+    <div
+      className={cn(
+        "relative rounded-md bg-slate-100 text-black border border-slate-200 ring-offset-1 transition-all",
+        "focus-within:ring-2"
+      )}
+    >
+      {prefix && (
+        <div className="absolute top-0 left-0 flex justify-center items-center w-8 h-full pointer-events-none">
+          {prefix}
+        </div>
+      )}
+      <input
+        type="search"
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className={cn(
+          prefix && "pl-8",
+          "w-full h-9 bg-transparent outline-none"
+        )}
+      />
+    </div>
+  );
+}
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  prefix: PropTypes.element,
+};
