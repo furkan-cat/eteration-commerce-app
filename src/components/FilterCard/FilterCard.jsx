1fbf9bc76c73d658049db1115e44d315ff13d158
Furkan Çat <furi544@gmail.com>
2023-05-17 20:42:57 +0300

    created custom filtercard component

---
+++ src/components/FilterCard/FilterCard.jsx
@@ -0,0 +1,70 @@
+import { useMemo, useState } from "react";
+import { BiSearchAlt } from "react-icons/bi";
+
+import { Card, CardBody, CheckBox, Radio } from "../../components";
+import Input from "../../components/Input/Input";
+
+export default function FilterCard({
+  data,
+  type,
+  name,
+  title,
+  showSearch,
+  onChange,
+}) {
+  const [text, setText] = useState("");
+
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const searchedData = useMemo(() => {
+    return data.filter((item) => {
+      if (typeof item === "object") {
+        return item.title.includes(text);
+      }
+      return item.includes(text);
+    });
+  }, [text]);
+
+  return (
+    <div>
+      {title ? <div>{title}</div> : null}
+      <Card>
+        {showSearch ? (
+          <Input prefix={<BiSearchAlt />} onChange={handleChangeText} />
+        ) : null}
+        <div className="max-h-36 overflow-y-scroll">
+          <CardBody>
+            {searchedData.map((filterValue) => {
+              const title = filterValue?.title || filterValue;
+              const value = filterValue?.value || filterValue;
+
+              if (type === "radio") {
+                return (
+                  <Radio
+                    key={value}
+                    name={name}
+                    title={title}
+                    value={value}
+                    onChange={onChange}
+                  />
+                );
+              } else {
+                return (
+                  <CheckBox
+                    key={value}
+                    name={name}
+                    title={title}
+                    value={value}
+                    onChange={onChange}
+                  />
+                );
+              }
+            })}
+          </CardBody>
+        </div>
+      </Card>
+    </div>
+  );
+}
