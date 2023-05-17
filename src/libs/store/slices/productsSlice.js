93dac64034c6448cf3eccde0c97dc71056c14aa3
Furkan Çat <furi544@gmail.com>
2023-05-17 20:46:52 +0300

    created productSlice to get data from the server. All sorting logic implemented in it.

---
+++ src/libs/store/slices/productsSlice.js
@@ -0,0 +1,80 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const fetchProductsAsync = createAsyncThunk(
+  "products/fetchProducts",
+  async (args) => {
+    const options = {
+      limit: 12,
+      skip: 0,
+      ...args,
+    };
+    try {
+      const originalData = await fetch(
+        "https://5fc9346b2af77700165ae514.mockapi.io/products"
+      ).then((res) => res.json());
+
+      let data = structuredClone(originalData);
+
+      if (options.sort_by) {
+        switch (args.sort_by) {
+          case "old-to-new":
+            data.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+            break;
+          case "new-to-old":
+            data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+            break;
+          case "price-high-to-low":
+            data.sort((a, b) => Number(b.price) - Number(a.price));
+            break;
+          case "price-low-to-high":
+            data.sort((a, b) => Number(a.price) - Number(b.price));
+            break;
+        }
+      }
+
+      if (options.model?.length) {
+        data = data.filter((product) => args.model.includes(product.model));
+      }
+
+      if (options.brand?.length) {
+        data = data.filter((product) => args.brand.includes(product.brand));
+      }
+
+      data = data.slice(
+        options.skip * options.limit,
+        options.skip * options.limit + options.limit
+      );
+
+      return { originalData, data };
+    } catch (error) {
+      throw error;
+    }
+  }
+);
+
+export const productsSlice = createSlice({
+  name: "products",
+  initialState: {
+    data: [],
+    originalData: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchProductsAsync.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchProductsAsync.fulfilled, (state, action) => {
+      state.data = action.payload.data;
+      if (!state.originalData.length)
+        state.originalData = action.payload.originalData;
+      state.loading = false;
+    });
+    builder.addCase(fetchProductsAsync.rejected, (state) => {
+      state.loading = false;
+    });
+  },
+});
+
+export default productsSlice.reducer;
