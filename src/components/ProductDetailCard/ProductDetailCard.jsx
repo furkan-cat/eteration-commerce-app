9fd3aca903b0e49d2d57cc3ef1a7b31cf8915dd7
Furkan Çat <furi544@gmail.com>
2023-05-17 20:43:26 +0300

    created custom productdetailcard component

---
+++ src/components/ProductDetailCard/ProductDetailCard.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+import Button from "../Button/Button";
+import cn from "classnames";
+
+export default function ProductDetailCard({ product, className }) {
+  return (
+    <div
+      className={cn(
+        "flex flex-col sm:flex-row gap-2 p-2.5 bg-white rounded-md",
+        className
+      )}
+    >
+      <img src={product.image} alt={product.name} className="w-full sm:w-1/2" />
+
+      <div className="w-full sm:w-1/2">
+        <div className="text-blue-600 truncate">{product.name}</div>
+        <div className="text-sm">
+          {product.brand} {product.model}
+        </div>
+        <Button title="Add to Cart" className="w-full" />
+        <p className="truncate">{product.description}</p>
+      </div>
+    </div>
+  );
+}
+
+ProductDetailCard.propTypes = {
+  className: PropTypes.string,
+  product: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    model: PropTypes.string,
+    brand: PropTypes.string,
+    id: PropTypes.string,
+    description: PropTypes.string,
+  }),
+};
