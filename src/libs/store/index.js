df3338c7f820dee3aeb39926047f8a720d7f8c99
Furkan Çat <furi544@gmail.com>
2023-05-18 16:54:19 +0300

    rehandled store, added subscription to store to get datas from localstorage if there is exist.

--- src/libs/store/index.js
+++ src/libs/store/index.js
@@ -1,10 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./slices/productsSlice";
 import filterReducer from "./slices/filterSlice";
+import cartReducer from "./slices/cartSlice";
+import { productDetailsApi } from "./api/productDetailsApi";
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
     filters: filterReducer,
+    cart: cartReducer,
+    [productDetailsApi.reducerPath]: productDetailsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productDetailsApi.middleware),
+
+  preloadedState: JSON.parse(localStorage.getItem("redux-state")) || {},
+});
+
+store.subscribe(() => {
+  const state = store.getState();
+
+  const localState = {
+    cart: state.cart,
+  };
+
+  localStorage.setItem("redux-state", JSON.stringify(localState));
 });
