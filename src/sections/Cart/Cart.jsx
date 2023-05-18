33435f122165ce3dd876d1f19eca392871f13eab
Furkan Çat <furi544@gmail.com>
2023-05-18 16:57:55 +0300

    card logic completed

--- src/sections/Cart/Cart.jsx
+++ src/sections/Cart/Cart.jsx
@@ -1,21 +1,53 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Card, CardBody, LineItem } from "../../components";
 import Button from "../../components/Button/Button";
+import {
+  resetCart,
+  totalPriceSelector,
+} from "../../libs/store/slices/cartSlice";
+import { useTimeout } from "../../libs/hooks";
 
 export default function Cart() {
+  const lineItems = useSelector((state) => state.cart.data);
+  const totalPrice = useSelector(totalPriceSelector);
+  const [isPending, setTimeout] = useTimeout();
+  const dispatch = useDispatch();
+
+  const handleCheckout = () => {
+    setTimeout(3000, () => dispatch(resetCart()));
+  };
+
+  // console.log(lineItems);
+
   return (
     <div className="grid gap-3">
       <Card>
         <CardBody>
-          <LineItem brand="Test" price="200" model="dssdsd" />
+          {lineItems.length > 0 ? (
+            lineItems.map((product) => {
+              return (
+                <div key={product.id}>
+                  {product.quantity > 0 && <LineItem product={product} />}
+                </div>
+              );
+            })
+          ) : (
+            <div className="text-sm">Cart is Empty!</div>
+          )}
         </CardBody>
       </Card>
 
       <Card>
         <CardBody className="grid gap-3">
           <div>
-            Total Price: <strong>117.000TL</strong>
+            Total Price: <strong>{totalPrice}₺</strong>
           </div>
-          <Button title="Checkout" className="w-full" />
+          <Button
+            title="Checkout"
+            className="w-full"
+            onClick={handleCheckout}
+            isLoading={isPending}
+          />
         </CardBody>
       </Card>
     </div>
