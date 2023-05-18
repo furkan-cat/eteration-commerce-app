8c31eb433b95f0495177c9a6b8e52516958130df
Furkan Çat <furi544@gmail.com>
2023-05-18 16:48:48 +0300

    improvement on button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,28 @@
 import PropTypes from "prop-types";
 import cn from "classnames";
+import Spinner from "../Spinner/Spinner";
 
-export default function Button({ title, children, type, onClick, className }) {
+export default function Button({
+  title,
+  children,
+  type,
+  onClick,
+  className,
+  disabled,
+  isLoading = false,
+}) {
   return (
     <button
       type={type}
       onClick={onClick}
+      disabled={disabled}
       className={cn(
-        "h-9 px-3 inline-flex justify-center items-center bg-blue-200 border border-blue-300 rounded-md text-blue-600 cursor-pointer",
+        "h-9 px-3 inline-flex whitespace-nowrap justify-center items-center bg-blue-500 border border-blue-300 rounded-md text-white text-sm cursor-pointer",
         "hover:opacity-80",
         className
       )}
     >
+      {isLoading ? <Spinner /> : null}
       {title || children}
     </button>
   );
@@ -22,5 +33,7 @@ Button.propTypes = {
   type: PropTypes.string,
   onClick: PropTypes.func,
   children: PropTypes.element,
+  disabled: PropTypes.bool,
+  isLoading: PropTypes.bool,
   className: PropTypes.string,
 };
