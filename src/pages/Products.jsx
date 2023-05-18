4482137dccb186058610238e379b5abce3f36a2b
Furkan Çat <furi544@gmail.com>
2023-05-18 16:57:31 +0300

    decrease, increase handlers added via rt methods

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,38 +1,68 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { fetchProductsAsync } from "../libs/store/slices/productsSlice";
+import {
+  decreasePage,
+  fetchProductsAsync,
+  increasePage,
+} from "../libs/store/slices/productsSlice";
 
 import ProductCard from "../components/ProductCard/ProductCard";
+import Pagination from "../components/Pagination/Pagination";
+import { addToCart } from "../libs/store/slices/cartSlice";
 
 export default function Products() {
   const dispatch = useDispatch();
-  const { data, loading } = useSelector((state) => state.products);
+  const { data, loading, meta, originalData } = useSelector((state) => state.products);
   const selectedFilters = useSelector((state) => state.filters.selectedFilters);
 
   useEffect(() => {
     dispatch(fetchProductsAsync(selectedFilters));
-  }, [selectedFilters]);
+  }, [selectedFilters, meta.page]);
+
+  const handleNext = () => {
+    dispatch(increasePage());
+  };
+  const handlePrev = () => {
+    dispatch(decreasePage());
+  };
+
+  const addToCartHandler = (e, products) => {
+    e.preventDefault();
+    dispatch(addToCart(products));
+  };
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="grid grid-cols-4">
-      {data.map((products) => (
-        <ProductCard
-          key={products.id}
-          product={{
-            id: products.id,
-            name: products.name,
-            price: products.price,
-            brand: products.brand,
-            model: products.model,
-            image: products.image,
-          }}
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+        {data.map((products) => (
+          <ProductCard
+            to={`/products/${products.id}`}
+            key={products.id}
+            product={{
+              id: products.id,
+              name: products.name,
+              price: products.price,
+              brand: products.brand,
+              model: products.model,
+              image: products.image,
+            }}
+            onClick={(e) => addToCartHandler(e, products)}
+          />
+        ))}
+      </div>
+      <div className="flex justify-center mt-5">
+        <Pagination
+          onNext={handleNext}
+          onPrev={handlePrev}
+          activePage={meta.page}
+          totalPage={Math.floor(originalData.length / 12)}
         />
-      ))}
-    </div>
+      </div>
+    </>
   );
 }
