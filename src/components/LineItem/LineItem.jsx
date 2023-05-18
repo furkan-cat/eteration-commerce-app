29a2adf29900c480a0950228255b9d83c467b5fa
Furkan Çat <furi544@gmail.com>
2023-05-18 16:50:22 +0300

    line item logic completed

--- src/components/LineItem/LineItem.jsx
+++ src/components/LineItem/LineItem.jsx
@@ -1,22 +1,41 @@
 import PropTypes from "prop-types";
 import QuantityPill from "../QuantityPill/QuantityPill";
+import { useDispatch } from "react-redux";
+import {
+  decreaseQuantity,
+  increaseQuantity,
+} from "../../libs/store/slices/cartSlice";
 
-export default function LineItem({ brand, model, price }) {
+export default function LineItem({ product }) {
+  const dispatch = useDispatch();
+
+  const handleChange = ({ type }) => {
+    if (type === "increase") {
+      dispatch(increaseQuantity(product.id));
+    } else {
+      dispatch(decreaseQuantity(product.id));
+    }
+  };
+
   return (
     <div className="flex items-center justify-between">
       <div className="flex flex-col">
         <div className="text-sm">
-          {brand} {model}
+          {product.brand} {product.model}
         </div>
-        <div className="text-sm text-blue-400">{price}</div>
+        <div className="text-sm text-blue-400">{product.price}</div>
       </div>
-      <QuantityPill value={3} onChange={console.log} />
+      <QuantityPill value={product.quantity} onChange={handleChange} />
     </div>
   );
 }
 
 LineItem.propTypes = {
-  brand: PropTypes.string.isRequired,
-  model: PropTypes.string.isRequired,
-  price: PropTypes.string.isRequired,
+  product: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    brand: PropTypes.string.isRequired,
+    model: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+  }),
 };
