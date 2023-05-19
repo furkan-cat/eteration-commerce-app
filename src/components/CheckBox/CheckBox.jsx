3a1960e3a9835f3184c5aa6a985faf9ca7a8f7ef
Furkan Çat <furi544@gmail.com>
2023-05-19 13:18:24 +0300

    defaultcheck prop added to checkbox component to make selectedfilters appear if user come url query strings

--- src/components/CheckBox/CheckBox.jsx
+++ src/components/CheckBox/CheckBox.jsx
@@ -1,17 +1,30 @@
 import PropTypes from "prop-types";
 
-export default function CheckBox({ title, name, value, onChange }) {
+export default function CheckBox({
+  name,
+  title,
+  value,
+  onChange,
+  defaultChecked,
+}) {
   return (
     <label className="flex space-x-2 text-sm py-0.5 cursor-pointer">
-      <input type="checkbox" name={name} value={value} onChange={onChange} />
+      <input
+        type="checkbox"
+        name={name}
+        value={value}
+        onChange={onChange}
+        defaultChecked={defaultChecked}
+      />
       {title && <span>{title}</span>}
     </label>
   );
 }
 
 CheckBox.propTypes = {
-  title: PropTypes.string,
   name: PropTypes.string,
+  title: PropTypes.string,
   value: PropTypes.string,
   onChange: PropTypes.func,
+  defaultChecked: PropTypes.bool
 };
